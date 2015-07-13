304adfab486fb629dbfe662e8c0e29ce51fadcde
Kyle Mitchell <kyle@kemitchell.com>
2015-07-12 22:41:23 -0700

    Use Droid Sans Mono

--- src/config.h
+++ src/config.h
@@ -62,7 +62,7 @@
 // #define DEFAULT_DIRECTORY      g_get_current_dir()
 // #define DEFAULT_TERMINAL_SIZE  80x24
 // #define EXPORT_WINDOWID        TRUE
-#define FONT "Monospace 12"
+#define FONT "Droid Sans Mono 12"
 // #define FONT                   "Monospace 10"
 #define FONT_ANTI_ALIAS TRUE
 // #define FONT_ANTI_ALIAS        TRUE
